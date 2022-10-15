0cb498c60b554f7e1df55470e905448aa950c3ca
GusCabraal <gustavocabral97@hotmail.com>
2022-10-15 11:43:31 -0300

    refactor: organização da rota login

--- src/routes/login.router.js
+++ src/routes/login.router.js
@@ -1,21 +1,15 @@
 const express = require('express');
-// const { read, getTalker } = require('../utils/readAndWriteFiles');
 const emailValidation = require('../middlewares/emailValidation');
 const passwordValidation = require('../middlewares/passwordValidation');
 const generateToken = require('../utils/generateToken');
 
-const loginRouter = express.Router();
-loginRouter.use(passwordValidation, emailValidation);
+const router = express.Router();
 
-loginRouter.post('/', async (request, response) => {
+router.use(passwordValidation, emailValidation);
+
+router.post('/', async (_request, response) => {
     const token = generateToken();
     response.status(200).json({ token });
 });
 
-// talkerRouter.get('/:id', talkerExists, async (request, response) => {
-//     const { id } = request.params;
-//     const talker = await getTalker(id);
-//     response.status(200).json(talker);
-// });
-
-module.exports = loginRouter;
+module.exports = router;
